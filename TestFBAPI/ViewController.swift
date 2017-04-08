c221c5fae6763215a3b28e0711487c3ed8c8f06f
Richard Du <richarddu@Richards-Mac.local>
2017-04-07 20:50:03 -0700

    Initial Commit

---
+++ TestFBAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFBAPI
+//
+//  Created by Richard Du on 4/7/17.
+//  Copyright © 2017 Richard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
